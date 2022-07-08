991f3ec013c90029849987ea8f454d18907b8f61
M Riezky Nurfajri <riezkynurfajri@gmail.com>
2022-07-08 20:20:27 +0700

    finish server side

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,12 +1,15 @@
 module.exports = function errorHandler(err, req, res, next) {
   let code = 500
   let message = "Internal Server Error"
-  console.log(err.name)
+  console.log(err.message)
 
   if (err.name === "SequelizeUniqueConstraintError" || err.name === "SequelizeValidationError") {
     code = 400
     const errors = err.errors.map((el) => el.message)
     message = errors.join(", ")
+  } else if (err.message === "invalid") {
+    code = 401
+    message = "Invalid Username / Password"
   }
 
   res.status(code).json({ code, message })
